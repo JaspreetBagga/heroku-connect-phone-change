5eff012462de761443ab80f7794a9171722c37fc
JaspreetBagga <78042377+JaspreetBagga@users.noreply.github.com>
2022-09-27 10:54:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,30 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/insert', function(req, res) {
+app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5,$6)',
-             [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim(),req.body.description.trim()],
-              function(err, result) {
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1,Description=$5 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim(),req.body.description.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5,$6)',
+                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim(),req.body.description.trim()],
+                  function(err, result) {
+                    done();
                     if (err) {
                         res.status(400).json({error: err.message});
                     }
                     else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
                     done();
                     res.json(result);
                 }
